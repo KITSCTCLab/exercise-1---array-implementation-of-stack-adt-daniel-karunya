d85d876e59231fabab5a9278b68e7e53e44acdd3
daniel-karunya <91770270+daniel-karunya@users.noreply.github.com>
2022-07-25 15:57:04 +0530

    Description Update

--- Main.py
+++ Main.py
@@ -1,38 +1,40 @@
 import os
+
+
 class Stack:
     """
     Description : The slass stack is used to implement stack adt using list functions.
-    
+
     Attributes :-
     self.items : list of items in stack
     items.size: max size of stack
-    
+
     """
+
     def __init__(self, size):
         self.items = []
         self.size = size
 
     def is_empty(self):
         """
-        Description : Checks if stack is empty 
+        Description : Checks if stack is empty
         Arguments : self
         Returns :true or false based on the result
-         
+
         """
-        if len(self.items) == 0 :
+        if len(self.items) == 0:
             return True
         else:
             return False
-
 
     def is_full(self):
         """
         Description : Checks if stack is full
         Arguments : self
         Returns : True or false based on the results
-         
+
         """
-        if len(self.items) == self.size :
+        if len(self.items) == self.size:
             return True
         else:
 
@@ -43,7 +45,7 @@ class Stack:
         Description : Adds data to top of stack based on the redult of the  isfull() function to check if stack is full
         Arguments : self,data
         Returns :data if push success
-         
+
         """
         if not self.is_full():
             self.items.append(data)
@@ -53,22 +55,24 @@ class Stack:
         Description : Removes data to top of stack based on the redult of the  isempty() function to check if stack is empty
         Arguments : self
         Returns :popped element
-         
+
         """
         popped = 0
         if not self.is_empty():
-            popped = self.items.pop(len(self.items)-1)
+            popped = self.items.pop(len(self.items) - 1)
         return popped
 
     def status(self):
+
         """
         Description : prints stack in order
         Arguments : self
         Returns :nil
-         
+
         """
-       for i in range (0,(len(self.items))):
-          print (self.items[(i)])
+        for i in range(0, (len(self.items))):
+            print(self.items[(i)])
+
 
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
