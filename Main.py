7390e942ca77e93b32f61d9f18363aeaeca2fd71
daniel-karunya <91770270+daniel-karunya@users.noreply.github.com>
2022-07-25 15:51:32 +0530

    Description update URK21CS1003-Daniel Gunasekaran

--- Main.py
+++ Main.py
@@ -1,10 +1,24 @@
 import os
 class Stack:
+    """
+    Description : The slass stack is used to implement stack adt using list functions.
+    
+    Attributes :-
+    self.items : list of items in stack
+    items.size: max size of stack
+    
+    """
     def __init__(self, size):
         self.items = []
         self.size = size
 
     def is_empty(self):
+        """
+        Description : Checks if stack is empty 
+        Arguments : self
+        Returns :true or false based on the result
+         
+        """
         if len(self.items) == 0 :
             return True
         else:
@@ -12,6 +26,12 @@ class Stack:
 
 
     def is_full(self):
+        """
+        Description : Checks if stack is full
+        Arguments : self
+        Returns : True or false based on the results
+         
+        """
         if len(self.items) == self.size :
             return True
         else:
@@ -19,16 +39,34 @@ class Stack:
             return False
 
     def push(self, data):
+        """
+        Description : Adds data to top of stack based on the redult of the  isfull() function to check if stack is full
+        Arguments : self,data
+        Returns :data if push success
+         
+        """
         if not self.is_full():
             self.items.append(data)
 
     def pop(self):
+        """
+        Description : Removes data to top of stack based on the redult of the  isempty() function to check if stack is empty
+        Arguments : self
+        Returns :popped element
+         
+        """
         popped = 0
         if not self.is_empty():
             popped = self.items.pop(len(self.items)-1)
         return popped
 
     def status(self):
+        """
+        Description : prints stack in order
+        Arguments : self
+        Returns :nil
+         
+        """
        for i in range (0,(len(self.items))):
           print (self.items[(i)])
 
